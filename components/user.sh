4e2aa255ef212da4278c46b62a292730972ccac0
phani <phanendrasai41@gmail.com>
2022-07-17 21:29:49 +0530

    updating ./Practice-roboshop

--- components/user.sh
+++ components/user.sh
@@ -2,14 +2,14 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 rm -rf user
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd catalogue
+unzip /tmp/user.zip
+mv user-main user
+cd user
 npm install
-sed -i -e 's/MONGO_ENDPOINT/mongodb-1.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-1.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+sed -i -e 's/MONGO_ENDPOINT/mongodb-1.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-1.roboshop.internal/' /home/roboshop/user/systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
+systemctl start user
+systemctl enable user
