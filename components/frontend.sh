6c38e3dfa5297f0887de70918caf5de44c1d2571
phani <phanendrasai41@gmail.com>
2022-07-17 20:35:10 +0530

    updating ./Practice-roboshop

--- components/frontend.sh
+++ components/frontend.sh
@@ -7,4 +7,7 @@ rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue-1.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
+
 
