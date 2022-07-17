b4c36b5c10ecd4995a591fa32ff398488db2f12f
phani <phanendrasai41@gmail.com>
2022-07-17 22:01:53 +0530

    updating ./Practice-roboshop

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,4 +1,14 @@
-
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue-1.roboshop.internal/' -e '/user/ s/localhost/user-1.roboshop.internal/' -e '/cart/ s/localhost/cart-1.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
 
 
 
