86bbd8b5ba37adf7d0cee48c7147fece7b51d795
phani <phanendrasai41@gmail.com>
2022-07-18 12:12:30 +0530

    updating ./Practice-roboshop

--- components/user.sh
+++ components/user.sh
@@ -1,1 +1,15 @@
-
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf user
+curl -s -L -o /tmp/user.zip https://github.com/roboshop-devops-project/user/archive/main.zip
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd user
+npm install
+sed -i -e 's/MONGO_ENDPOINT/mongodb-1.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-1.roboshop.internal/' /home/roboshop/user/systemd.service
+mv /home/roboshop/user/systemd.service  /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
