dfabfa06bc322791abef4e7292391cf5ab6fff4e
phani <phanendrasai41@gmail.com>
2022-07-18 12:59:42 +0530

    updating ./Practice-roboshop

--- components/mysql.sh
+++ components/mysql.sh
@@ -3,14 +3,10 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 MYSQL_DEFAULT_PASSWORD=$( grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql
+mysql  -uroot -p"${MYSQL_DEFAULT_PASSWORD}" </tmp/mysql
 
-curl -s -L -o /tmp/mysql.zip https://github.com/roboshop-devops-project/mysql/archive/main.zip
-cd /tmp
-unzip -o mysql.zip
-cd mysql-main
-mysql -uroot -p"${MYSQL_PASSWORD}" <shipping.sql
+
 
 
 
