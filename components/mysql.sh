a090b1a7a362350e2eb3bf9c0a3b0d8559f598c2
phani <phanendrasai41@gmail.com>
2022-07-18 05:34:24 +0530

    updating ./Practice-roboshop

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,1 +1,12 @@
-echo Install Mysql Component
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+
+
+#curl -s -L -o /tmp/mysql.zip https://github.com/roboshop-devops-project/mysql/archive/main.zip
+#cd /tmp
+#unzip -o mysql.zip
+#cd mysql-main
+#mysql -uroot -p"${MYSQL_PASSWORD}" <shipping.sql
+
