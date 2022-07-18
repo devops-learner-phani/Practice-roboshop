cfaa46e159f2493164c6d54f2c55b67d409686f0
phani <phanendrasai41@gmail.com>
2022-07-18 12:20:24 +0530

    updating ./Practice-roboshop

---
+++ components/cart.sh
@@ -0,0 +1,15 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf cart
+curl -s -L -o /tmp/cart.zip https://github.com/roboshop-devops-project/cart/archive/main.zip
+cd /home/roboshop
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+sed -i -e 's/CATALOGUE_ENDPOINT/catalogue-1.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-1.roboshop.internal/' /home/roboshop/cart/systemd.service
+mv /home/roboshop/cart/systemd.service  /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
