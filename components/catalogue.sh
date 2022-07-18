1bd5a80acbd8ebf89608cc02b417b8dd63510a1f
phani <phanendrasai41@gmail.com>
2022-07-18 11:54:56 +0530

    updating ./Practice-roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,3 +1,17 @@
-
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf catalogue
+curl -s -L -o /tmp/catalogue.zip https://github.com/roboshop-devops-project/catalogue/archive/main.zip
+cd /home/roboshop
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd catalogue
+npm install
+sed -i -e 's/MONGO_DNSNAME/mongodb-1.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+mv /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue  
 
 
