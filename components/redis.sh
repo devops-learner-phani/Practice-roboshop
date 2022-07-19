c0a80a8a86fa6c120e411b78fb92bd6ae30f2831
phani <phanendrasai41@gmail.com>
2022-07-19 11:13:40 +0530

    updating ./Practice-roboshop

--- components/redis.sh
+++ components/redis.sh
@@ -1,10 +1,13 @@
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo you are Non root user
-    echo you have to run the script as a root user or run the script with sudo
-   exit 1
-fi
+CHECK_ROOT() {
+  USER_ID=$(id -u)
+  if [ $USER_ID -ne 0 ]; then
+      echo you are Non root user
+      echo you have to run the script as a root user or run the script with sudo
+     exit 1
+  fi
+}
 
+CHECK_ROOT
 
 
 curl -L -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo
