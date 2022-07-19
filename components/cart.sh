d2a2159627a4e4dbef1d0c059500236ae4bb24ee
phani <phanendrasai41@gmail.com>
2022-07-19 11:30:35 +0530

    updating ./Practice-roboshop

--- components/cart.sh
+++ components/cart.sh
@@ -3,6 +3,12 @@ source components/common.sh
 CHECK_ROOT
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+if [ $? -ne 0 ]; then
+  echo -e  "\e[31mPRINT it is a FAILURE\e[0m"
+else
+  echo -e "\e[32mSUCCESS\e[0m"
+fi
+
 yum install nodejs -y
 useradd roboshop
 rm -rf cart
