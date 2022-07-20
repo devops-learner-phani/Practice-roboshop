778c172de4e797006b7e8eba3354ae445aa500d2
phani <phanendrasai41@gmail.com>
2022-07-20 14:34:12 +0530

    updating ./Practice-roboshop

--- components/common.sh
+++ components/common.sh
@@ -10,3 +10,12 @@ CHECK_ROOT() {
 LOG=/tmp/roboshop.log
 rm -r ${LOG}
 
+CHECK_STAT() {
+if [ $1 -nq 0 ]; then
+  echo -e "\e[31mFAILED\e[0m"
+  exit 2
+else
+   echo -e "\e[32mSUCCESS\e[0m"
+fi
+
+}
