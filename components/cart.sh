30080ca7089b550706640feeefd660a5f500cf87
phani <phanendrasai41@gmail.com>
2022-07-20 17:10:59 +0530

    updating ./Practice-roboshop

--- components/cart.sh
+++ components/cart.sh
@@ -14,7 +14,9 @@ PRINT "Update application user"
 id roboshop &>>${LOG}
 if [ $? -ne 0 ]; then
   useradd roboshop &>>${LOG}
+fi
 CHECK_STAT $?
+
 
 PRINT "Download cart content"
 curl -s -L -o /tmp/cart.zip https://github.com/roboshop-devops-project/cart/archive/main.zip &>>${LOG}
