1941d19770e1d4d38ec44468b769f070d5f77dec
phani <phanendrasai41@gmail.com>
2022-07-20 21:24:33 +0530

    updating ./Practice-roboshop

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -15,7 +15,8 @@ systemctl enable mongod &>>${LOG} && systemctl start mongod &>>${LOG} && systemc
 CHECK_STAT $?
 
 PRINT "Setup mongodb configuration"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf systemctl enable mongod &>>${LOG} && systemctl start mongod &>>${LOG} && systemctl status mongod &>>${LOG}
+
 CHECK_STAT $?
 
 PRINT "Restart mongodb service"
