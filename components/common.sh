1cad4a5f51e71119023e09a46f563b7caa408066
phani <phanendrasai41@gmail.com>
2022-07-20 14:50:19 +0530

    updating ./Practice-roboshop

--- components/common.sh
+++ components/common.sh
@@ -15,6 +15,7 @@ rm -r $LOG
 CHECK_STAT() {
 if [ $1 -ne 0 ]; then
   echo -e "\e[31mFAILED\e[0m"
+  echo -e "\n check log file - ${LOG} for errors \n"
   exit 2
 else
   echo -e "\e[32mSUCCESS\e[0m"
